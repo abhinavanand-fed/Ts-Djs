1f4879c3246ba9fb9cf47d293a7fc9a56408906f
abhinavanand-fed <abhinavanand080@gmail.com>
2024-03-07 00:39:52 +0530

    ICustomClient

--- src/base/interfaces/ICustomClient.ts
+++ src/base/interfaces/ICustomClient.ts
@@ -1,8 +1,14 @@
+import { Collection } from "discord.js";
 import { IConfig } from "./IConfig";
+import SubCommand from "../classes/SubCommand";
+import Command from "../classes/Command";
 
 export default interface ICustomClient {
     config: IConfig;
-
+    commands: Collection<string, Command>;
+    subCommands: Collection<string, SubCommand>;
+    cooldowns: Collection<string, Collection<string, number>>;
+    
     Init(): void;
     LoadHandlers(): void;
 }
