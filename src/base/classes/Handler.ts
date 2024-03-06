2628245c9d361b2ff5a260cb80295bb2fd1a10a5
abhinavanand-fed <abhinavanand080@gmail.com>
2024-03-07 00:38:57 +0530

    Handler

--- src/base/classes/Handler.ts
+++ src/base/classes/Handler.ts
@@ -3,13 +3,14 @@ import path from "path";
 import { glob } from "glob";
 import CustomClient from "./CustomClient";
 import Event from "./Event";
+import Command from "./Command";
+import SubCommand from "./SubCommand";
 
 export default class Handler implements IHandler {
     client: CustomClient;
     constructor(client: CustomClient) {
         this.client = client;
     }
-    void: any;
 
     async LoadEvents() {
         const files = (await glob(`build/event/**/*.js`)).map(filePath => path.resolve(filePath));
@@ -26,6 +27,26 @@ export default class Handler implements IHandler {
             if(event.once) this.client.once(event.name, execute);
             //@ts-ignore
             else this.client.on(event.name, execute);
+
+            return delete require.cache[require.resolve(file)];
+
+        });
+    }
+
+    async LoadCommands() {
+        const files = (await glob(`build/commands/**/*.js`)).map(filePath => path.resolve(filePath));
+
+        files.map(async (file: string) => {
+            const command: Command | SubCommand = new(await import(file)).default(this.client);
+
+            if(!command.name)
+                return delete require.cache[require.resolve(file)] && console.log(`${file.split("/").pop()} does not have a name.`);
+
+
+            if (file.split("/").pop()?.split(".")[2])
+               return this.client.subCommands.set(command.name, command);
+
+            this.client.commands.set(command.name, command as Command);
 
             return delete require.cache[require.resolve(file)];
 
