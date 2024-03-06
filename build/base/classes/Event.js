59391f2b18e6a9c522021d8b6e63d9678c866801
abhinavanand-fed <abhinavanand080@gmail.com>
2024-03-06 20:07:29 +0530

    new commit | Typescript bot

---
+++ build/base/classes/Event.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class default_1 {
+    constructor(client, options) {
+        this.client = client;
+        this.name = options.name;
+        this.description = options.description;
+        this.once = options.once;
+    }
+    Execute(...args) { }
+    ;
+}
+exports.default = default_1;
